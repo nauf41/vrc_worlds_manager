76e49826238b86f06d65d484408cef1abdae304d
nowf41 <111325821+nowf41@users.noreply.github.com>
2026-04-26 18:37:13 +0900

    ADD tauri app

---
+++ world_manager/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      
+    </main>
+  );
+}
+
+export default App;
