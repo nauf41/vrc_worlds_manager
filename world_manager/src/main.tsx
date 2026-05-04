28e2de83dfdf8577507c46513e57dc78bccc0d45
nowf41 <111325821+nowf41@users.noreply.github.com>
2026-05-04 22:28:29 +0900

    REF re-configured UI for shadcn

--- world_manager/src/main.tsx
+++ world_manager/src/main.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./views/App";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
